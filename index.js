cca77aabcda781996ee215d0f2078b3ab7ee72a5
Mihir Chaturvedi <plibither8@gmail.com>
2020-05-20 12:04:14 +0530

    fix: use fastify instead

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const fetch = require('node-fetch')
+const fastify = require('fastify')({ trustProxy: false });
 
 const {TG_BOT_NAME, TG_BOT_SECRET} = process.env
 async function sendToTelegram(message) {
@@ -16,7 +17,47 @@ async function sendToTelegram(message) {
 	})
 }
 
+fastify.get('/', async (req, res) => {
+	const {ip} = req;
+	const ipDetails = await fetch(`http://ip-api.com/json/${ip}?fields=4255257`)
+		.then(res => res.json());
+	await sendToTelegram(
+`🎯 *Ping test: success!*
+
+*IP Details:*
+*IP:* \`${ip}\`
+*ISP:* ${ipDetails.isp}
+*Org:* ${ipDetails.org}
+*AS:* ${ipDetails.as}
+*AS Name:* ${ipDetails.asName}
+*City:* ${ipDetails.city}
+*Region:* ${ipDetails.regionName}
+`)
+	res.send('success')
+});
+
+fastify.post('/', async (req, res) => {
+	const {ip, body} = req;
+	const ipDetails = await fetch(`http://ip-api.com/json/${ip}?fields=4255257`)
+		.then(res => res.json());
+	await sendToTelegram(
+`🎯 *Ping test: success!*
+
+*IP Details:*
+*IP:* \`${ip}\`
+*ISP:* ${ipDetails.isp}
+*Org:* ${ipDetails.org}
+*AS:* ${ipDetails.as}
+*AS Name:* ${ipDetails.asName}
+*City:* ${ipDetails.city}
+*Region:* ${ipDetails.regionName}
+
+*Request body:*
+${body}
+`)
+	res.send('success')
+});
+
 module.exports = async (req, res) => {
-	await sendToTelegram('🎯 Ping test: success!')
-	res.end('success')
+	console.log(Object.keys(req))
 }
